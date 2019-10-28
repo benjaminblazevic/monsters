1aeaaf78726b621f9cdd26ee8b5e871a418ba07d
Benjamin Blazevic <benjaminblae13@gmail.com>
2019-10-29 00:00:09 +0100

    Updates

---
+++ precache-manifest.6774485148f44a63fa3d915a50ea76e7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0654bc45b73ed5814041f47f0857d3a2",
+    "url": "/monsters/index.html"
+  },
+  {
+    "revision": "fb88c006ba25514269c5",
+    "url": "/monsters/static/css/main.8e8353c9.chunk.css"
+  },
+  {
+    "revision": "1767bf201d46f191fce6",
+    "url": "/monsters/static/js/2.02b0cce3.chunk.js"
+  },
+  {
+    "revision": "fb88c006ba25514269c5",
+    "url": "/monsters/static/js/main.0b06ef15.chunk.js"
+  },
+  {
+    "revision": "b08f6d7fc5fcb3b49104",
+    "url": "/monsters/static/js/runtime-main.fdd2b06c.js"
+  }
+]);
